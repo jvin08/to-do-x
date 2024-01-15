5ae54afb9c0e14f6ba961af833591a5d99837cd0
jvin08 <jvin08@gmail.com>
2024-01-15 11:36:31 -0600

    useParams

--- src/components/Pages.styles.ts
+++ src/components/Pages.styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { FaRegEdit } from "react-icons/fa";
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 500px;
